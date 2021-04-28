fc4f657f0c01b9575fb6647dca61b8e2c680bffc
lhernandez19 <diegotapia1987@gmail.com>
2021-04-27 21:25:09 -0500

    Completed!

--- src/edu/wctc/independentcopy/reader/ListReader.java
+++ src/edu/wctc/independentcopy/reader/ListReader.java
@@ -1,9 +1,24 @@
 package edu.wctc.independentcopy.reader;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListReader implements Reader{
 
     @Override
     public String readln() {
-        return null;
+        List<String> myArray = new ArrayList<String>();
+        myArray.add("Here");
+        myArray.add("is");
+        myArray.add("my");
+        myArray.add("array");
+
+        String output = "";
+
+        for (int i = 0; i < myArray.size(); i++)
+        {
+            output += myArray.get(i) + " ";
+        }
+        return output;
     }
 }
